ca13f9c3e7c51c7d983b3b25287fe819f3f8f020
Luiza <luiza-97@hotmail.com>
2022-09-14 09:53:05 -0300

    Get e Post de Postagem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,8 @@ import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
 import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
 import { FormsModule } from '@angular/forms';
 import { FeedComponent } from './feed/feed.component';
+import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.component';
+import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delete.component';
 
 @NgModule({
   declarations: [
@@ -27,7 +29,9 @@ import { FeedComponent } from './feed/feed.component';
     TemaComponent,
     TemaEditComponent,
     TemaDeleteComponent,
-    FeedComponent
+    FeedComponent,
+    PostagemEditComponent,
+    PostagemDeleteComponent
   ],
   imports: [
     BrowserModule,
--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -3,6 +3,10 @@ import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
 import { TemaService } from '../service/tema.service';
 import { Tema } from '../model/Tema';
+import { Postagem } from '../model/Postagem';
+import { AuthService } from '../service/auth.service';
+import { PostagemService } from '../service/postagem.service';
+import { User } from '../model/User';
 
 @Component({
   selector: 'app-feed',
@@ -18,20 +22,33 @@ export class FeedComponent implements OnInit {
   usuario = environment.usuario
 
   tema: Tema = new Tema()
+  postagem: Postagem = new Postagem()
+  user: User = new User()
+
   listaTemas: Tema[]
+  listaPostagem: Postagem[]
+
+  idTema: number
+
+  idUser = environment.id
 
   constructor(
     private router: Router,
-    private temaService: TemaService
+    private temaService: TemaService,
+    private postagemService: PostagemService,
+    private authService: AuthService
   ) { }
 
   ngOnInit() {
+    window.scroll(0,0)
 
     if (environment.token == '') {
       this.router.navigate(['/inicio'])
     }
 
     this.findAllTemas()
+    this.getAllTemas()
+    this.getAllPostagens()
   }
 
   findAllTemas() {
@@ -55,5 +72,46 @@ export class FeedComponent implements OnInit {
       },
     })
 
+  }
+
+  getAllTemas(){
+    this.temaService.getAllTemas().subscribe((resp: Tema[]) => {
+      this.listaTemas = resp
+    })
+  }
+
+  findByIdTema(){
+    this.temaService.getByIdTema(this.idTema).subscribe((resp: Tema) =>{
+      this.tema = resp
+    })
+  }
+
+  getAllPostagens(){
+    this.postagemService.getAllPostagens().subscribe((resp: Postagem[]) =>{
+      this.listaPostagem = resp
+    })
+  }
+
+  findByIdUser(){
+    this.authService.getByIdUser(this.idUser).subscribe((resp: User) => {
+      this.user = resp
+    })
+  }
+
+  publicar(){
+    this.tema.id = this.idTema
+    this.postagem.tema = this.tema
+
+    this.user.id = this.idUser
+    this.postagem.usuario = this.user
+
+    this.postagemService.postPostagem(this.postagem).subscribe((resp: Postagem) => {
+      this.postagem = resp
+
+      alert('Postagem realizada com sucesso')
+
+      this.postagem = new Postagem()
+      this.getAllPostagens()
+    })
   }
 }
