711cf2ecfbc94df9b7db796ba7ccbda91c3d68c7
luanaadn <luanaadn@hotmail.com>
2022-09-14 11:11:52 -0300

    Edit-delete-postagem inserido

--- src/app/edit/postagem-edit/postagem-edit.component.ts
+++ src/app/edit/postagem-edit/postagem-edit.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Postagem } from 'src/app/model/Postagem';
+import { Tema } from 'src/app/model/Tema';
+import { PostagemService } from 'src/app/service/postagem.service';
+import { TemaService } from 'src/app/service/tema.service';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-postagem-edit',
@@ -7,9 +13,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostagemEditComponent implements OnInit {
 
-  constructor() { }
+  postagem: Postagem = new Postagem()
+  tema: Tema = new Tema()
 
-  ngOnInit(): void {
+  listaTemas: Tema[]
+  idTema: number
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private postagemService: PostagemService,
+    private temaService: TemaService
+  ) { }
+
+  ngOnInit() {
+    window.scroll(0,0)
+
+    if(environment.token == ''){
+      // alert('Sua sessão expirou! Faça login novamente!')
+      this.router.navigate(['/entrar'])
+    }
+
+    let id = this.route.snapshot.params['id']
+    this.findByIdPostagem(id)
+    this.findAllTemas()
   }
 
+  findByIdPostagem(id: number) {
+    this.postagemService.getByIdPostagem(id).subscribe((resp: Postagem) => {
+      this.postagem = resp
+    })
+  }
+
+  findByIdTema() {
+    this.temaService.getByIdTema(this.idTema).subscribe((resp: Tema) => {
+      this.tema = resp
+    })
+  }
+
+  findAllTemas() {
+    this.temaService.getAllTemas().subscribe((resp: Tema[]) => {
+      this.listaTemas = resp
+    })
+  }
+
+  atualizar() {
+    if (this.idTema == null) {
+      const element = document.querySelector("#tema");
+      if(element != null){
+        element.insertAdjacentHTML('afterend', '<p class="text-danger font-weight-bold mt-2 mb-2">Tema é obrigatório</p>');
+      }
+    }
+    else {
+      this.tema.id = this.idTema
+      this.postagem.tema = this.tema
+
+      this.postagemService.putPostagem(this.postagem).subscribe((resp: Postagem) => {
+        this.postagem = resp
+        alert('Postagem atualizada com sucesso!')
+        this.router.navigate(['/feed'])
+      })
+  }
+
+}
 }
