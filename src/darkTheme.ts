8017a03717d663a2b1aabd446e84629561767dfb
Luiza <luiza-97@hotmail.com>
2022-09-19 12:52:49 -0300

    dark mode atualizado

--- src/darkTheme.ts
+++ src/darkTheme.ts
@@ -3,6 +3,11 @@ export function toggleDarkTheme(){
     document.body.classList.toggle('bg-secundary')
     document.getElementById('btn-theme')?.classList.toggle('dark-theme')
     document.getElementById('ball')?.classList.toggle('dark-theme')
+    document.getElementsByClassName('color-nav')[0]?.classList.toggle('dark-theme')
+    var elements = document.getElementsByClassName('font-theme')
+    for(var i = 0; i< elements.length; i++){
+      elements[i].classList.toggle('dark-theme')
+    }
     cardDarkTheme()
     modalTheme()
   }
